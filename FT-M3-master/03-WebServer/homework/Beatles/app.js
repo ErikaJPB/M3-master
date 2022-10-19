3a21e2c7c80897a3f795add3f8499bebd1762dee
ErikaJPB <erikajpinedab@gmail.com>
2022-10-19 12:08:23 -0500

    Homework

--- FT-M3-master/03-WebServer/homework/Beatles/app.js
+++ FT-M3-master/03-WebServer/homework/Beatles/app.js
@@ -22,3 +22,49 @@ var beatles=[{
   profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
 }
 ]
+
+
+http.createServer((request, response) => {
+
+  if(request.url === "/" ) {
+    const index = fs.readFileSync("./index.html");
+
+    response.writeHead(200, {"Content-type":"text/html"})
+    return response.end(index)
+  }
+  
+  const myUrl = request.url.split("/");
+  
+  if (myUrl.length < 3  && !myUrl.includes('api')) {
+    const beatleName = myUrl[1].replace("%20", " "); // decodeURI quita el %20 // encodeURI
+    const OneBeatle = beatles.filter((elem) => elem.name === beatleName)[0] // para que devuelva solo el objeto
+
+
+    let template = fs.readFileSync("./beatle.html", "utf-8")
+    template = template.replace("{name}", OneBeatle.name)
+    template = template.replace("{birthdate}", OneBeatle.birthdate)
+    template = template.replace("{image}", OneBeatle.profilePic)
+    
+    response.writeHead(200, {"Content-type":"text/html"})
+    return response.end(template)
+  }
+
+
+  if (request.url === "/api") {
+   response.writeHead(200, { "Content-type":"application/json"})
+   return response.end(JSON.stringify(beatles));
+  }
+
+  const beatleName = request.url.split("/").pop().replace("%20", " ") 
+  if (request.url.includes("/api") && beatleName) {
+    const OneBeatle = beatles.filter((elem) => elem.name === beatleName);
+
+    if (!OneBeatle.length) {
+    response.writeHead(404, {"Content-type": "text/plain"})
+    return response.end("Beatle not found")
+  }
+    response.writeHead(200, { "Content-type": "application/json"})
+    return response.end(JSON.stringify(OneBeatle[0]))
+  }
+  
+}).listen(3002, "localhost")
--- FT-M3-master/03-WebServer/homework/showByName/app.js
+++ FT-M3-master/03-WebServer/homework/showByName/app.js
@@ -1,4 +1,32 @@
-var fs  = require("fs")
-var http  = require("http")
+var fs = require("fs")
+var http = require("http")
 
 // Escribí acá tu servidor
+
+ http.createServer((request, response) => {
+        const name = request.url.slice(1);
+        const files = fs.readdirSync("./images");
+
+        for (const file of files) {
+            if (file.includes(name)) {
+                response.writeHead(200, { "Content-type": "image/jpg" });
+                const img = fs.readFileSync(`./images/${name}_doge.jpg`);
+                return response.end(img);
+            }
+        }
+        return response.writeHead(404).end("Not found")
+    }).listen(3001, "localhost");
+
+
+
+// http.createServer((request, response) => {
+//     fs.readFile(`./images${request.url}.jpg`, (error, data) => {
+//         if (error) {
+//             response.writeHead(404, {"Content-type": "text/plain"})
+//             response.end("Image not found")
+//         } else {
+//             response.writeHead(200, {"Content-type": "image/jpeg"})
+//             response.end(data)
+//         }
+//     })
+// }).listen(3003, "localhost")
